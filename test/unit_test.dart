6082b0908f9dd0ec6fef149399499bc8e83dbf75
Amaury <amaury84@gmail.com>
2026-05-05 18:47:27 -0500

    doc: abcd

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -8,7 +8,7 @@ void main(){
       final usuario = User(nombre: "Julian",rol: "Admin");
     //operaciones abcd
       
-    //asserts
+    //asserts abcd
     expect(usuario.nombre, "Julian");
 
   });
