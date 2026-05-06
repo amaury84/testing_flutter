52bcb0d174e59c13ceeb213678b87347bd10e091
Amaury <amaury84@gmail.com>
2026-05-05 20:38:26 -0500

    test comentario 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:testing_flutter/screens/pantalla.dart';
 
 void main() {//comentario nuevo
   runApp(const MainApp());
-}//comentario
+}//comentario nuevo 2
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
